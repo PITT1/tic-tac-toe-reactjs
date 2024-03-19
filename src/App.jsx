57c6ab1ae3f7fc71e9259e52c75ac52cfb8533f1
PITT1 <petroleo.plta.97@gmail.com>
2024-03-18 22:36:43 -0400

    estilando los cuadrados

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import './App.css'
 
+const turns = {
+  x: "x",
+  o: "o"
+}
+
+const board = Array(9).fill(null);
+
+const Square = ({ children, updateBoard, index}) => {
+  return(
+    <div className='border border-solid border-white text-5xl text-center h-full flex justify-center items-center'>
+      {children}
+    </div>
+  )
+}
+
 function App() {
-  const turns = {
-    x: "x",
-    o: "o"
-  }
-
-  const board = Array(9).fill(null);
-
   return (
     <>
-    <h1 className='text-3xl'>TIC-TAC-TOE</h1>
-    <section className='grid grid-cols-3 grid-rows-3'>{
-    board.map((_, index) => {
-      return(
-        <div key={index}>
-          <span>
-            {index}
-          </span>
-        </div>
-      )
-    })
-    }</section>
+      <h1 className='text-3xl'>TIC-TAC-TOE</h1>
+      <section className='h-96 grid grid-cols-3 grid-rows-3 aspect-square justify-items-stretch items-stretch gap-1'>{
+        board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              {index}
+            </Square>
+          )
+        })
+      }</section>
     </>
   )
 }
